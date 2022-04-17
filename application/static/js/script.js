ed212ffb828cc98ab646d83c3c01e17e80d7082d
mohamed <mohamed.baioumy1@outlook.com>
2022-04-17 14:47:34 -0300

    edit js code

--- application/static/js/script.js
+++ application/static/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 $(document).ready(() => {
   const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-  // const passwordPattern=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/;
+  const passwordPattern=/^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
 
 
   $("#registration_form").submit(evt => {
@@ -27,10 +27,10 @@ $(document).ready(() => {
          isValid = false;
      }
 
-    // else if  (!passwordPattern.test(pass)) {
-    //   $("#pass").next().text("Must be valid Password");
-    //   isValid = false;
-    // }
+    else if  (!passwordPattern.test(pass)) {
+      $("#pass").next().text("Must be valid Password");
+      isValid = false;
+    }
      else {
       pass.next().text("");
     }
